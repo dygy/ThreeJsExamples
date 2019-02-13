6429c7e374a3df4c2b3d765ad239fee009e5d684
Dygy <Yukimurachris@gmail.com>
2019-02-13 20:58:51 +0300

    fixes from hackathon

--- examples/4/js/client_world.js
+++ examples/4/js/client_world.js
@@ -1,12 +1,7 @@
-
-const ws = new WebSocket('ws://localhost:40511');
 let lastMessage;
+let ready;
+let started = '';
 // event emmited when connected
-ws.onopen = () =>{
-    console.log('websocket is connected ...');
-    // sending a send event to websocket server
-    antiSpam('CONNECT')
-};
 
 let time;
 let todo;
@@ -32,6 +27,7 @@ let boxWinMaterial;
 let boxWin;
 let goal;
 let boxWinSize;
+let boxWinHeight;
 camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.02, 40 );
 camera.position.set( 0, 2, - 2 );
 scene = new THREE.Scene();
@@ -43,14 +39,14 @@ function loadWorld() {
     todo = '';
     stop = 0.30;
     stopSize = 1;
-    DEGTORAD = 0.015;
+    DEGTORAD = 0.053;
     temp = new THREE.Vector3();
     boxSize.x=5;
     boxSize.y=1;
     boxSize.z=5;
     cubeSize.xyz=0.2;
-
-
+    cubeSize.height=0.1;
+    boxWinHeight = 0.1;
     init();
     animate();
 }
@@ -77,14 +73,20 @@ function init() {
     // add the plane to the scene
     scene.add(box);
 
-    createWin(cube);
+    createWin();
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
 }
-function createWin(cube){
-    boxWinSize= Math.round((Math.random()*5));
+function createWin(){
+    boxWinSize = Math.ceil((Math.random()*5));
+    if (boxWinSize === 0){ boxWinSize=1;}
+    let toClalc = boxWinSize;
+    while (toClalc >0.1){
+        boxWinHeight= boxWinHeight+0.05;
+        toClalc= toClalc-0.1
+    }
     boxWinGeometry = new THREE.BoxBufferGeometry();
     boxWinMaterial = new THREE.MeshNormalMaterial();
     boxWin         = new THREE.Mesh(boxWinGeometry, boxWinMaterial);
@@ -92,7 +94,9 @@ function createWin(cube){
     boxWin.material.transparent = true;
     boxWin.material.opacity = 0.7;
     boxWin.scale.multiplyScalar(boxWinSize);
-    boxWin.position.set(Math.abs(Math.random()*boxSize.x/2),cube.position.y,Math.abs(Math.random()*boxSize.z/2));
+    boxWin.position.x = Math.abs(Math.random()*boxSize.x/2);
+    boxWin.position.z = Math.abs(Math.random()*boxSize.z/2);
+    boxWin.position.y = boxWinHeight;
     boxWin.rotation.set(0,0,0);
     // add the plane to the scene
     scene.add(boxWin);
@@ -114,8 +118,8 @@ document.addEventListener("keydown", event => {
 });
 
 function moveW() {todo='UP';}
-function moveA() {todo='LEFT'}
-function moveD() {todo='RIGHT'}
+function moveA() {todo='LEFT';}
+function moveD() {todo='RIGHT';}
 function moveS() {todo='DOWN';}
 function moveE() {todo='UPRIGHT';}
 function moveQ() {todo='UPLEFT';}
@@ -123,154 +127,136 @@ function moveZ() {todo='DOWNLEFT';}
 function moveX() {todo='DOWNRIGHT';}
 function smaller() {todo='SMALLER';}
 function bigger() {todo='BIGGER';}
-function antiSpam(message) {
-    if (message !== lastMessage ){
-        lastMessage=message;
-    ws.send(message)
-    }
-}
+
 
 function animate() {
-    console.log(cubeSize.xyz+' '+boxWinSize);
+    console.log(cubeSize.xyz+' '+' '+ cube.position.x+' '+boxWinSize+' '+ boxWin.position.x);
     if (Math.abs(cube.position.x)>boxSize.x/2||
         Math.abs(cube.position.z)>boxSize.z/2) {todo='FALL';}
-    if (cubeSize.xyz===boxWinSize && cube.position===boxWinSize.position){
+    if (cubeSize.xyz===boxWinSize && (cube.position.x===boxWin.position.x ||cube.position.x===boxWin.position.z)){
         alert('You won!');
-        scene.remove(boxWin);
-        createWin(cube);
     }
     requestAnimationFrame(animate);
-
-    if (todo==='SMALLER'){
-        time+=0.01;
+    if (todo === 'SMALLER') {
+        time += 0.01;
         if (time < stopSize) {
             if (cubeSize.xyz > 0.1) {
-                cubeSize.xyz = Math.round((cubeSize.xyz - 0.1) *10)/10;
-                cube.scale.set(cubeSize.xyz, cubeSize.xyz, cubeSize.xyz)
-
+                cubeSize.xyz = Math.round((cubeSize.xyz - 0.1) * 10) / 10;
+                cube.scale.set(cubeSize.xyz, cubeSize.xyz, cubeSize.xyz);
+                cubeSize.height = cubeSize.height - 0.05;
+                cube.position.y = cubeSize.height;
             }
         }
-        todo='';
+        todo = '';
         time = 0;
     }
 
-    if (todo==='BIGGER') {
-        time+=0.01;
+    else if (todo === 'BIGGER') {
+        time += 0.01;
         if (time < stopSize) {
             if (cubeSize.xyz < boxSize.x) {
-                cubeSize.xyz = Math.round((cubeSize.xyz + 0.1) *10)/10;
+                cubeSize.xyz = Math.round((cubeSize.xyz + 0.1) * 10) / 10;
                 cube.scale.set(cubeSize.xyz, cubeSize.xyz, cubeSize.xyz);
-
+                cubeSize.height = cubeSize.height + 0.05;
+                cube.position.y = cubeSize.height;
             }
         }
-        todo='';
+        todo = '';
         time = 0;
     }
-
-    if (todo==='UP'){
-        time+=0.01;
+    else if (todo === 'UP') {
+        time += 0.01;
         cube.translateZ(0.01);
         if (time > stop) {
-            todo='';
+            todo = '';
             time = 0;
             cube.translateZ(0.00);
         }
     }
-    if (todo==='LEFT'){
-        time+=0.01;
+    else if (todo === 'LEFT') {
+        time += 0.01;
         cube.rotateY(DEGTORAD);
         if (time > stop) {
-            todo='';
+            todo = '';
             time = 0;
         }
     }
-    if (todo==='DOWN'){
-        time+=0.01;
+    else if (todo === 'DOWN') {
+        time += 0.01;
         cube.translateZ(-0.01);
         if (time > stop) {
-            todo='';
+            todo = '';
             time = 0;
             cube.translateZ(0.00);
         }
     }
-    if (todo==='RIGHT'){
-        time+=0.01;
+    else if (todo === 'RIGHT') {
+        time += 0.01;
         cube.rotateY(-DEGTORAD);
         if (time > stop) {
-            todo='';
+            todo = '';
             time = 0;
         }
     }
-    if (todo==='DOWNRIGHT'){
-        time+=0.01;
-        cube.rotateY(DEGTORAD);
-        cube.translateZ(-0.01);
-        if (time > stop) {
-            todo='';
-            time = 0;
-        }
-    }
-    if (todo==='DOWNLEFT'){
-        time+=0.01;
-        cube.rotateY(-DEGTORAD);
-        cube.translateZ(-0.01);
-        if (time > stop) {
-            todo='';
-            time = 0;
-        }
-    }
-    if (todo==='UPLEFT'){
-        time+=0.01;
-        cube.rotateY(-DEGTORAD);
-        cube.translateZ(0.01);
-        if (time > stop) {
-            todo='';
-            time = 0;
-        }
-    }
-    if (todo==='UPRIGHT'){
-        time+=0.01;
-        cube.rotateY(DEGTORAD);
-        cube.translateZ(0.01);
-        if (time > stop) {
-            todo='';
-            time = 0;
-        }
-    }
+    else if (todo === 'DOWNRIGHT') {
+        started='DOWNRIGHT'
 
-    if (todo==='FALL'){
-        time+=0.01;
+        time += 0.01;
+        cube.rotateY(DEGTORAD);
+        cube.translateZ(-0.01);
+        if (time > stop) {
+            todo = '';
+            time = 0;
+        }
+    }
+    else if (todo === 'DOWNLEFT') {
+        started='DOWN'
+        time += 0.01;
+        cube.rotateY(-DEGTORAD);
+        cube.translateZ(-0.01);
+        if (time > stop) {
+            todo = '';
+            time = 0;
+        }
+    }
+    else if (todo === 'UPLEFT') {
+        time += 0.01;
+        cube.rotateY(-DEGTORAD);
+        cube.translateZ(0.01);
+        if (time > stop) {
+            todo = '';
+            time = 0;
+
+        }
+    }
+    else if (todo === 'UPRIGHT') {
+        time += 0.01;
+        cube.rotateY(DEGTORAD);
+        cube.translateZ(0.01);
+        if (time > stop) {
+            todo = '';
+            time = 0;
+        }
+    }
+    else if (todo === 'FALL') {
+        time += 0.01;
         cube.rotateY(DEGTORAD);
         cube.translateY(-0.1);
         if (time > stop) {
-            todo='';
+            todo = '';
             time = 0;
-
         }
-       antiSpam('FALL');
-        setTimeout(()=> {location.reload();},3000);
+
+        setTimeout(() => {
+            location.reload();
+        }, 3000);
 
     }
+
 
     temp.setFromMatrixPosition(goal.matrixWorld);
     camera.position.lerp(temp, 0.2);
     camera.lookAt(cube.position);
     renderer.render(scene, camera);
-    ws.onmessage = response => {
-        let message =response.data;
-        if (message===('MOVE_W'))          moveW();
-        else if (message==='MOVE_S')       moveS();
-        else if (message ==='MOVE_A')      moveA();
-        else if (message ==='MOVE_D')      moveD();
-        else if (message ==='MOVE_SA')     moveZ();
-        else if (message ==='MOVE_SD')     moveX();
-        else if (message ==='MOVE_WD')     moveE();
-        else if (message ==='MOVE_WA')     moveQ();
-        else if (message ==='CUBE_BIGGER') bigger();
-        else if (message ==='CUBE_SMALLER')smaller();
-        console.log('Server sends you a " '+ response.data +' " command');
-    };
+
 }
-
-
-
